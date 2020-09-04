74b86fb3c143acd037d9d2417118d783c2c141c4
JMercie <themercie@gmail.com>
2020-09-04 01:16:49 -0300

    add allow credentials to be able to pass cookie

--- main/main.js
+++ main/main.js
@@ -13,8 +13,9 @@ app.use(
   })
 )
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Credentials' ,'true')
   next();
 });
 
