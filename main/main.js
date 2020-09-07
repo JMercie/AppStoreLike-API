05be1d80a81d4879e52d188d1758fb1ead23268d
JMercie <themercie@gmail.com>
2020-09-07 17:35:38 -0300

    allow new headers from client

--- main/main.js
+++ main/main.js
@@ -14,7 +14,7 @@ app.use(
 )
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
   res.header('Access-Control-Allow-Credentials' ,'true')
   next();
 });
